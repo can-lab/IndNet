d2efc58987915f2f35e5769f64d4f71f9931ac2b
Florian Krause <siebenhundertzehn@gmail.com>
2018-10-31 12:08:44 +0100

    Update example.py

--- example.py
+++ example.py
@@ -24,8 +24,8 @@ core_networks.base_dir = "/path/to/base_directory/"  # set working/output direct
 
 # Create indnet node
 indnet = create_indnet_workflow(smoothing=5, hp_cutoff=100, threshold=0.66, aggr_aroma=False)
-indnet.inputs.inputspec.t1_file = "/path/to/t1.nii"  # point to anatomical T1 scan (NiFTI file)
-indnet.inputs.inputspec.rs_file = "/path/to/rs.nii"  # point to functional resting state scan (NiFTI file)
+indnet.inputs.inputspec.anat_file = "/path/to/t1.nii"  # point to anatomical T1 scan (NiFTI file)
+indnet.inputs.inputspec.func_file = "/path/to/rs.nii"  # point to functional resting state scan (NiFTI file)
 TEMPL_DIR = os.path.abspath("./Functional_ROIs")  # point to FIND template directory
 indnet.inputs.inputspec.templates = [
     os.path.join(TEMPL_DIR, 'anterior_Salience', "anterior_Salience.nii.gz"),
@@ -53,14 +53,28 @@ indnet.inputs.inputspec.networks = [
 results = Node(io.DataSink(parameterization=False), name='results')
 
 # Connect indnet and output nodes
-core_networks.connect(indnet, 'outputspec.networks_main_files',
-                      results, 'networks.main')
-core_networks.connect(indnet, 'outputspec.networks_exclusive_files',
-                      results, 'networks.exclusive')
-core_networks.connect(indnet, 'outputspec.preprocessed_rs_file',
+core_networks.connect(indnet, 'outputspec.network_masks_func_main',
+                      results, 'networks.func.main')
+core_networks.connect(indnet, 'outputspec.network_masks_func_exclusive',
+                      results, 'networks.func.exclusive')
+core_networks.connect(indnet, 'outputspec.network_masks_anat_main',
+                      results, 'networks.anat.main')
+core_networks.connect(indnet, 'outputspec.network_masks_anat_exclusive',
+                      results, 'networks.anat.exclusive')
+core_networks.connect(indnet, 'outputspec.network_masks_mni_main',
+                      results, 'networks.mni.main')
+core_networks.connect(indnet, 'outputspec.network_masks_mni_exclusive',
+                      results, 'networks.mni.exclusive')
+core_networks.connect(indnet, 'outputspec.preprocessed_func_file',
                       results, 'preprocessed.rs')
-core_networks.connect(indnet, 'outputspec.preprocessed_t1_file',
+core_networks.connect(indnet, 'outputspec.preprocessed_anat_file',
                       results, 'preprocessed.t1')
+core_networks.connect(indnet, 'outputspec.motion_parameters',
+                      results, 'motion_parameters')
+core_networks.connect(indnet, 'outputspec.func2anat_transform',
+                      results, 'func2anat_transform')
+core_networks.connect(indnet, 'outputspec.anat2mani_transform',
+                      results, 'anat2mni_transform')
 
 
 # Run workflow
